0011fb8c72684b057a647bacf49a0be55beed5f8
Arseni <senchik549@gmail.com>
2022-12-11 15:06:18 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
 
-      <Link to="">
+      <Link to="" >
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/sneakers/4.jpg" alt="Logo" />
           <div>
