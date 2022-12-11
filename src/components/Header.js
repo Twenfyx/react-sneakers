d4ed61c6e0445053880f4f28184e658e2e770475
Arseni <senchik549@gmail.com>
2022-12-11 15:03:42 +0100

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
 
-      <Link to="/">
+      <Link to="">
         <div className="d-flex align-center">
-          <img width={40} height={40} src={"img/logo.png"} alt="Logo" />
+          <img width={40} height={40} src="img/sneakers/4.jpg" alt="Logo" />
           <div>
             <h3 className="text-uppercase">React Sneakers</h3>
             <p className="opacity-5">Shop of the best Sneakers</p>
